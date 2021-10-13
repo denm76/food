8d7df36d7be074d7e5c3b012160b30bbe4ad6f90
denm76 <denm76@gmail.com>
2021-10-13 18:02:40 +0300

    Реализован таймер при помощи обновления значения полей в setInterval

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Tabs
     const   tabs = document.querySelectorAll('.tabheader__item'),
             tabsContent = document.querySelectorAll('.tabcontent',),
             tabsParent = document.querySelector('.tabheader__items');
@@ -36,5 +37,64 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
-      
+
+
+    //Timer
+
+    // вид input type='date' - '2021-09-09'
+    const deadline = '2021-12-12';
+
+    //Функция, подсчитывающая остаток времени до дедлайна и
+    //передающая его в объекте по компонентам времени
+    function getTimeRemaining(endtime){
+        const now = new Date();
+        const t = Date.parse(endtime) - Date.parse(now),
+              days = Math.floor(t/(1000*60*60*24)),
+              hours = Math.floor((t/(1000*60*60)) % 24),
+              minutes = Math.floor((t/1000/60) % 60),
+              seconds = Math.floor((t/1000) % 60);
+
+        return{
+            'total':t,
+            'days':days,
+            'hours':hours,
+            'minutes':minutes,
+            'seconds':seconds
+        }
+    }
+
+    function getZero(num){
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        }
+        else{
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        //Запускаем функцию обновления до запуска setInterval во избежания мегания таймера на странице
+        updateClock();
+
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if(t.total <= 0){
+                 clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setClock('.timer', deadline);
 });
